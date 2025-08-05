b1b29fe4bfceeb4292c08524c37909d70795c887
kyzov <vlad.meison@gmail.com>
2025-08-05 10:49:26 +0300

    update_tests

--- ConsoleApp1/TestProject1/UnitTest1.cs
+++ ConsoleApp1/TestProject1/UnitTest1.cs
@@ -19,25 +19,25 @@ namespace TestProject1
         [Test]
         public void Add_SimpleValues_ReturnsSum()
         {
-            Assert.AreEqual(5, calc.Add(2, 3));
+            Assert.That(calc.Add(2, 3), Is.EqualTo(5));
         }
 
         [Test]
         public void Subtract_SimpleValues_ReturnsDifference()
         {
-            Assert.AreEqual(1, calc.Subtract(3, 2));
+            Assert.That(calc.Subtract(3, 2), Is.EqualTo(1));
         }
 
         [Test]
         public void Multiply_SimpleValues_ReturnsProduct()
         {
-            Assert.AreEqual(6, calc.Multiply(2, 3));
+            Assert.That(calc.Multiply(2, 3), Is.EqualTo(6));
         }
 
         [Test]
         public void Divide_SimpleValues_ReturnsQuotient()
         {
-            Assert.AreEqual(2, calc.Divide(6, 3));
+            Assert.That(calc.Divide(6, 3), Is.EqualTo(2));
         }
 
         [Test]
