3b3a4ccbcd97d8cf3440a482dca71d04f27ebfcb
kyzov <vlad.meison@gmail.com>
2025-08-05 10:55:59 +0300

    78

--- ConsoleApp1/TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
+++ ConsoleApp1/TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ad068a559d31735624e80c4920984ee9fcc40e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1b29fe4bfceeb4292c08524c37909d70795c887")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
